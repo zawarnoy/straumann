05e503d68e90553d5244150569d79e6403dc1867
Vlad <vlad@useresponse.com>
2019-04-24 15:38:46 +0300

    buttons, small fixes

--- resources/views/parts/menu.blade.php
+++ resources/views/parts/menu.blade.php
@@ -1,7 +1,9 @@
 <div class="logo"><a href="{{ url('/') }}"><img src="{{ asset('img/site_logo.svg') }}" alt="logo"></a></div>
 <nav>
     <i class="fa fa-bars menu-bar" aria-hidden="true"></i>
+
     <ul>
+        <i class="fa fa-times close-menu" aria-hidden="true"></i>
         {!!  menu('frontend') !!}
         {{--@foreach(menu('frontend', '_json') as $item)--}}
             {{--@php(error_log(print_r(get_object_vars($item), 1), 3,'log.txt'))--}}

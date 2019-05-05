0c55b6ac44b7870c51b6fb4c25203dd67f00e05b
Vlad <vlad@useresponse.com>
2019-05-05 12:14:14 +0300

    button-blocks

--- resources/views/choice.blade.php
+++ resources/views/choice.blade.php
@@ -13,12 +13,14 @@
             <div class="role-block patient-block">
                 <div class="role-block-image"
                      style="background-image: url({{ Voyager::image(setting('choice.patient_image'))}})">
+                    <div class="choice-button float-right">Я пациент</div>
                 </div>
             </div>
 
             <div class="role-block doctor-block">
                 <div class="role-block-image"
                      style="background-image: url({{ Voyager::image(setting('choice.doctor_image')) }})">
+                    <div class="choice-button float-left">Я стоматолог</div>
                 </div>
             </div>
 
